9268d085fa4ed1d3d76d09ab8603fb23011a520c
Bishnupada Kamila <121252447+bishnu-kamila@users.noreply.github.com>
2024-10-16 17:37:55 +0530

    update code

--- src/Component/Login.jsx
+++ src/Component/Login.jsx
@@ -1,7 +1,16 @@
 const Login = ()=>{
     return(
         <>
-            <h1>Welcome to login page</h1>
+            <h1>Login</h1>
+            <p>Welcome Back !, Please enter your details</p>
+            <div>
+                <div>
+                    <label>Email</label>
+                    <input type="email" placeholder="Enter yor Email_id" />
+                    <label>Password</label>
+                    <input type="password" />
+                </div>
+            </div>
         </>
     )
 }
