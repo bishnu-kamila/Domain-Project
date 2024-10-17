7321a73eb6c109554fc06314d865105606b121b2
Bishnupada Kamila <121252447+bishnu-kamila@users.noreply.github.com>
2024-10-17 21:18:22 +0530

    Loginpage

--- src/Component/Login.jsx
+++ src/Component/Login.jsx
@@ -1,16 +1,30 @@
 const Login = ()=>{
     return(
         <>
-            <h1>Login</h1>
-            <p>Welcome Back !, Please enter your details</p>
-            <div>
-                <div>
-                    <label>Email</label>
-                    <input type="email" placeholder="Enter yor Email_id" />
-                    <label>Password</label>
-                    <input type="password" />
+            <div className="flex items-center justify-center h-screen ">
+                <div className="border-2 bg-gray-300 px-10 py-10 rounded-3xl border-2 border-gray">
+                    <h1 className="text-5xl font-semibold">Login</h1>
+                    <p className="font-medium text-lg mt-4">Welcome Back !, Please enter your details</p>
+                    <div className="mt-8">
+                        <div>
+                            <div className="flex">
+                                <label className="font-medium text-lg border-grey-100 rounded-xl p-4 mt-1 " >Email</label>
+                                <input className="w-full h-8 border-2 border-grey-100 rounded-xl p-4 mt-4 ml-8" type="email" placeholder="Enter yor Email_id" />
+                            </div>
+                            <div className="flex">
+                                <label className="font-medium text-lg border-grey-100 rounded-xl p-4 mt-1">Password</label>
+                                <input className="w-full h-8 border-2 border-grey-100 rounded-xl p-4 mt-4" type="password" placeholder="Password" />
+                            </div>
+                        </div>
+                        <div className="">
+                            <button className="mt-8 ml-[30px] w-[130px]">ForgetPassword</button>
+                            <button className="mt-8 ml-[60px] bg-blue-300 rounded-md w-[70px]">Login</button>
+                        </div>
+                    </div>
+                    
                 </div>
             </div>
         </>
     )
 }
+export default Login
--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,10 @@ import App from './App.jsx'
 import './index.css'
 import Navbar from './Component/Navbar.jsx'
 import Signup from './Component/Signup.jsx'
+import Login from './Component/Login.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Signup />
+    <Login/>
   </StrictMode>,
 )
