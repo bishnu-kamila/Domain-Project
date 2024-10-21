1c6d6104337b2edad9c58bd72d1f081460863436
Bishnupada Kamila <121252447+bishnu-kamila@users.noreply.github.com>
2024-10-21 23:13:30 +0530

    New Component

---
+++ src/Component/Dashbord.jsx
@@ -0,0 +1,7 @@
+const Dashbord = ()=>{
+    return(
+        <>
+            <h1>This is Dashboard</h1>
+        </>
+    )
+}
--- src/Component/Signup.jsx
+++ src/Component/Signup.jsx
@@ -3,6 +3,7 @@ const Signup = () => {
   return (
       <>
         <div className="flex items-center justify-center h-screen ">
+            
                 <div className="border-2 bg-gray-300 px-10 py-10 rounded-3xl border-2 border-gray">
                     <h1 className="text-5xl font-semibold">Register</h1>
                     <p className="font-medium text-lg mt-4">Welcome Back !, Please enter your details</p>
