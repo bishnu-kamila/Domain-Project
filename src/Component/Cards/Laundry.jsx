0cb99bde65095d80c8bf39981d80ad92dea2b415
Bishnupada Kamila <121252447+bishnu-kamila@users.noreply.github.com>
2024-11-01 21:16:03 +0530

    Update

--- src/Component/Cards/Laundry.jsx
+++ src/Component/Cards/Laundry.jsx
@@ -1,7 +1,7 @@
 const Laundry = ()=>{
     return(
         <>
-            <div>
+            <div classname="h-[50px] w-[35px]">
                 <h1>This is Laundry Page</h1>
             </div>
         </>
