c4de1a4629c06ae865438d6f05a35777e507c171
Bishnupada Kamila <121252447+bishnu-kamila@users.noreply.github.com>
2024-11-22 23:34:40 +0530

    update

--- src/Component/Laundry.jsx
+++ src/Component/Laundry.jsx
@@ -2,6 +2,10 @@ const Laundry = ()=>{
     return(
         <>
             <h1>This is Laundry page</h1>
+            <div className="flex items-center justify-center h-screen">
+                <button>Book Laundry</button>
+                <button>Book Premium Laundry</button>
+            </div>
         </>
     )
 }
