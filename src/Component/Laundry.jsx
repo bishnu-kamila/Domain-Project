fb42cd2d5605c17753c488097da4246bccdacab6
Bishnupada Kamila <121252447+bishnu-kamila@users.noreply.github.com>
2024-12-05 23:43:56 +0530

    Update Laundry.jsx
    Update

--- src/Component/Laundry.jsx
+++ src/Component/Laundry.jsx
@@ -5,6 +5,7 @@ const Laundry = ()=>{
             <div className="flex items-center justify-center h-screen">
                 <button className="border-2 bg-blue h-[20px] w-[50px]">Book Laundry</button>
                 <button className="border-2 bg-blue h-[20px] w-[70px]">Book Premium Laundry</button>
+                <h1>order inquary</h1>
             </div>
         </>
     )
