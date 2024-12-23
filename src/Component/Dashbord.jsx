73bf26f678dae224bc507755b5e95ace8c7ad3d5
Bishnupada Kamila <121252447+bishnu-kamila@users.noreply.github.com>
2024-12-23 13:29:06 +0530

    Update

--- src/Component/Dashbord.jsx
+++ src/Component/Dashbord.jsx
@@ -6,8 +6,9 @@ const Dashbord = ()=>{
                 <h1 className="text-6xl font-bold  flex item-center justify-center mt-[50px]">Dashboard</h1>
                 <div className="flex item-center justify-center mt-[70px]">
                     <div>
-                    <div className="h-[200px] w-[200px] ml-[60px] bg-blue-400 flex justify-center items-center rounded-[40px]">
-                        <h1 className="text-center font-white text-4xl">
+                    <div className="h-[200px] w-[200px] ml-[60px] bg-blue-400  justify-center items-center rounded-[40px]">
+                        <img src="./HostelImgg.png" alt="" />
+                        <h1 className="text-center font-white text-xl">
                             Hostel
                         </h1>
                     </div>
@@ -19,36 +20,43 @@ const Dashbord = ()=>{
                     
                     </div>
                     <div>
-                    <div className="h-[200px] w-[200px] bg-red-400 ml-[60px] flex justify-center items-center rounded-[40px]">
-                        <h1 className="text-center font-white text-4xl"> 
+                    <div className="h-[200px] w-[200px]  ml-[60px]  justify-center items-center rounded-[40px]">
+
+                        <img src="./notificationimg.png" alt="" />
+                        <h1 className="text-center font-white text-xl"> 
                             Notification
                         </h1>
                     </div>
-                    <div className="h-[200px] w-[200px] bg-green-400 ml-[60px] mt-[50px] flex justify-center items-center rounded-[40px]">
-                        <h1 className="text-center font-white text-4xl">Canteen</h1>
+                    <div className="h-[200px] w-[200px] ml-[60px] mt-[50px] justify-center items-center rounded-[40px]">
+                        <img src="./CanteenImg.png" alt="" />
+                        <h1 className="text-center font-white text-xl">Canteen</h1>
                     </div>
                     </div>
                     <div>
-                    <div className="h-[200px] w-[200px] bg-gray-400 ml-[60px] flex justify-center items-center rounded-[40px]">
-                        <h1 className="text-center font-white text-4xl">
+                    <div className="h-[200px] w-[200px]  ml-[60px] justify-center items-center rounded-[40px]">
+                        <img src="./Laundryy.png" alt="" />
+                        <h1 className="text-center font-white text-xl">
                             Laundry
                         </h1>
                     </div>
-                    <div className="h-[200px] w-[200px] bg-purple-700 ml-[60px] mt-[50px] flex justify-center items-center rounded-[40px]">
-                        <h1 className="text-center font-white text-4xl">
+                    <div className="h-[200px] w-[200px]  ml-[60px] mt-[50px] justify-center items-center rounded-[40px]">
+                        <img src="./Feedback.png" alt="" />
+                        <h1 className="text-center font-white text-xl">
                             Feedback
                         </h1>
                     </div>
                     </div>
 
                     <div>
-                    <div className="h-[200px] w-[200px] bg-blue-200 ml-[60px] flex justify-center items-center rounded-[40px]">
-                        <h1 className="text-center font-white text-4xl">
+                    <div className="h-[200px] w-[200px] bg-blue-200 ml-[60px] justify-center items-center rounded-[40px]">
+                        <img src="./RoomImg.png" alt="" />
+                        <h1 className="text-center font-white text-xl">
                             Room Info
                         </h1>
                     </div>
-                    <div className="h-[200px] w-[200px] bg-lime-400 ml-[60px] mt-[50px] flex justify-center items-center rounded-[40px]">
-                        <h1 className="text-center font-white text-4xl">
+                    <div className="h-[200px] w-[200px]  ml-[60px] mt-[50px] justify-center items-center rounded-[40px]">
+                        <img src="./PaymentImg.png" alt="" />
+                        <h1 className="text-center font-white text-xl">
                             Payment
                         </h1>
                     </div>
