933c34edb2baa9875040e9b8ffffce234c6c5a6a
Bishnupada Kamila <121252447+bishnu-kamila@users.noreply.github.com>
2025-01-02 16:45:44 +0530

    Update

--- src/Component/Dashbord.jsx
+++ src/Component/Dashbord.jsx
@@ -1,69 +1,79 @@
-import React from "react"
-const Dashbord = ()=>{
-    return(
-        <>
-            <div className="">
-                <h1 className="text-6xl font-bold  flex item-center justify-center mt-[50px]">Dashboard</h1>
-                <div className="flex item-center justify-center mt-[70px]">
-                    <div>
-                    <div className="h-[200px] w-[200px] ml-[60px] bg-blue-400  justify-center items-center rounded-[40px]">
-                        <img src="./HostelImgg.png" alt="" />
-                        <h1 className="text-center font-white text-xl">
-                            Hostel
-                        </h1>
-                    </div>
-                    <div className="h-[200px] w-[200px] bg-yellow-400 ml-[60px] mt-[50px] flex justify-center items-center rounded-[40px]">
-                        <h1 className="text-center font-white text-4xl">
-                            Registration
-                        </h1>
-                    </div>
-                    
-                    </div>
-                    <div>
-                    <div className="h-[200px] w-[200px]  ml-[60px]  justify-center items-center rounded-[40px]">
+import React from "react";
+import { Link } from "react-router-dom";
 
-                        <img src="./notificationimg.png" alt="" />
-                        <h1 className="text-center font-white text-xl"> 
-                            Notification
-                        </h1>
-                    </div>
-                    <div className="h-[200px] w-[200px] ml-[60px] mt-[50px] justify-center items-center rounded-[40px]">
-                        <img src="./CanteenImg.png" alt="" />
-                        <h1 className="text-center font-white text-xl">Canteen</h1>
-                    </div>
-                    </div>
-                    <div>
-                    <div className="h-[200px] w-[200px]  ml-[60px] justify-center items-center rounded-[40px]">
-                        <img src="./Laundryy.png" alt="" />
-                        <h1 className="text-center font-white text-xl">
-                            Laundry
-                        </h1>
-                    </div>
-                    <div className="h-[200px] w-[200px]  ml-[60px] mt-[50px] justify-center items-center rounded-[40px]">
-                        <img src="./Feedback.png" alt="" />
-                        <h1 className="text-center font-white text-xl">
-                            Feedback
-                        </h1>
-                    </div>
-                    </div>
-
-                    <div>
-                    <div className="h-[200px] w-[200px] bg-blue-200 ml-[60px] justify-center items-center rounded-[40px]">
-                        <img src="./RoomImg.png" alt="" />
-                        <h1 className="text-center font-white text-xl">
-                            Room Info
-                        </h1>
-                    </div>
-                    <div className="h-[200px] w-[200px]  ml-[60px] mt-[50px] justify-center items-center rounded-[40px]">
-                        <img src="./PaymentImg.png" alt="" />
-                        <h1 className="text-center font-white text-xl">
-                            Payment
-                        </h1>
-                    </div>
-                    </div>
-                </div>
+const Dashbord = () => {
+  return (
+    <div>
+      <h1 className="text-6xl font-bold flex items-center justify-center mt-[50px]">
+        Dashboard
+      </h1>
+      <div className="flex items-center justify-center mt-[70px] flex-wrap gap-[50px]">
+        <div>
+          <Link to="/hostel">
+            <div className="h-[200px] w-[200px] justify-center items-center rounded-[40px] flex flex-col">
+              <img src="./HostelImgg.png" alt="Hostel" />
+              <h1 className="text-center text-black text-xl">Hostel</h1>
             </div>
-        </>
-    )
-}
-export default Dashbord
+          </Link>
+
+          <Link to="/registration">
+            <div className="h-[200px] w-[200px] mt-[50px] justify-center items-center rounded-[40px] flex flex-col">
+              <img src="./Registration Img.png" alt="Registration" />
+              <h1 className="text-center text-black text-xl">Registration</h1>
+            </div>
+          </Link>
+        </div>
+
+        <div>
+          <Link to="/notification">
+            <div className="h-[200px] w-[200px] justify-center items-center rounded-[40px] flex flex-col">
+              <img src="./notificationimg.png" alt="Notification" />
+              <h1 className="text-center text-black text-xl">Notification</h1>
+            </div>
+          </Link>
+
+          <Link to="/canteen">
+            <div className="h-[200px] w-[200px] mt-[50px] justify-center items-center rounded-[40px] flex flex-col">
+              <img src="./CanteenImg.png" alt="Canteen" />
+              <h1 className="text-center text-black text-xl">Canteen</h1>
+            </div>
+          </Link>
+        </div>
+
+        <div>
+          <Link to="/laundry">
+            <div className="h-[200px] w-[200px] justify-center items-center rounded-[40px] flex flex-col">
+              <img src="./Laundryy.png" alt="Laundry" />
+              <h1 className="text-center text-black text-xl">Laundry</h1>
+            </div>
+          </Link>
+
+          <Link to="/feedback">
+            <div className="h-[200px] w-[200px] mt-[50px] justify-center items-center rounded-[40px] flex flex-col">
+              <img src="./Feedback.png" alt="Feedback" />
+              <h1 className="text-center text-black text-xl">Feedback</h1>
+            </div>
+          </Link>
+        </div>
+
+        <div>
+          <Link to="/room-info">
+            <div className="h-[200px] w-[200px] justify-center items-center rounded-[40px] flex flex-col">
+              <img src="./RoomImg.png" alt="Room Info" />
+              <h1 className="text-center text-black text-xl">Room Info</h1>
+            </div>
+          </Link>
+
+          <Link to="/payment">
+            <div className="h-[200px] w-[200px] mt-[50px] justify-center items-center rounded-[40px] flex flex-col">
+              <img src="./PaymentImg.png" alt="Payment" />
+              <h1 className="text-center text-black text-xl">Payment</h1>
+            </div>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashbord;
--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,6 +1,11 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
 const Home = ()=>{
     return(
         <>
+        <Navbar/>
+            <Outlet/>
+
         <div >
             <img className="h-[650px] w-[1500px] mt-[20px]" src="./src/images/hostel.jpg" alt="" />
             
