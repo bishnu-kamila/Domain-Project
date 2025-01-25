f0c4076e6eebc581cecb93df5a7748d388929b11
Bishnupada Kamila <121252447+bishnu-kamila@users.noreply.github.com>
2025-01-25 23:17:40 +0530

    update

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,22 +1,35 @@
 import React from "react";
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
-    return(
+    return (
         <>
-            <div className="bg-blue h-[30px] w-fit flex ml-[20px]">
-                <img className="h-[50px] w-[40px]" src="./src/images/images-removebg-preview.png" alt="" />
-                <h1 className="text-black ml-[30px] mt-[10px] font-bold">Hostel Management System</h1>
-                <div className="ml-[750px] mt-[10px]">
-                    <Link to="/">Home</Link>
-                    <a className="ml-[30px]" href="">Education</a>
-                    <a className="ml-[30px]" href="">About</a>
+            <div className="bg-blue h-[30px] w-full flex items-center justify-between p-4">
+                <div className="flex items-center">
+                    <img className="h-[50px] w-[40px]" src="./src/images/images-removebg-preview.png" alt="" />
+                    <h1 className="text-black ml-[20px] text-lg font-bold">Hostel Management System</h1>
                 </div>
-                <div className="ml-[50px] mt-[10px]">
-                    <Link to = "/signup">Sign-up</Link>
-                    <Link to = "/login" className="ml-[20px]">Sign-in</Link>
+
+                {/* Navigation links for large screens */}
+                <div className="hidden md:flex space-x-6">
+                    <Link to="/">Home</Link>
+                    <a href="">Education</a>
+                    <a href="">About</a>
+                </div>
+
+                {/* Sign-in and Sign-up links */}
+                <div className="hidden md:flex space-x-6">
+                    <Link to="/signup">Sign-up</Link>
+                    <Link to="/login" className="ml-[20px]">Sign-in</Link>
+                </div>
+
+                {/* Mobile Menu Button */}
+                <div className="md:hidden">
+                    <button className="text-white">☰</button>
                 </div>
             </div>
         </>
-    )
-}
+    );
+};
+
 export default Navbar;
