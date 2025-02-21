92769b7cc3997eee943f31260e420e7804f324ef
Bishnupada Kamila <121252447+bishnu-kamila@users.noreply.github.com>
2025-02-21 22:52:39 +0530

    update

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <div
       className="mt-[20px] h-screen w-screen bg-cover bg-center flex items-center justify-center"
-      style={{ backgroundImage: "url('https://thumbs.dreamstime.com/b/beds-hostel-affordable-housing-36997317.jpg')" }} // Replace with your image URL
+      style={{ backgroundImage: "url('https://thumbs.dreamstime.com/b/beds-hostel-affordable-housing-36997317.jpg')" }} 
     >
       <div className="bg-black bg-opacity-50 p-10 rounded-lg text-center">
         <h1 className="text-5xl font-bold text-white mb-4">Hostel Management System</h1>
