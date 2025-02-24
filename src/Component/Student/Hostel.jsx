9efc033e42a7bd58c5aef582f3149b79a7817f39
Bishnupada Kamila <121252447+bishnu-kamila@users.noreply.github.com>
2025-02-24 23:56:47 +0530

    update

--- src/Component/Student/Hostel.jsx
+++ src/Component/Student/Hostel.jsx
@@ -5,16 +5,7 @@ const Hostel = ()=>{
                 <h1 className="font-bold text-2xl">Hostel</h1>
             </div>
             <div className="flex display-center justify-center mt-[30px]">
-                <div>
-                    <div>
-                        <label>Enter Hostel number</label>
-                        <input type="text" placeholder="Hostel number :-" />
-                    </div>
-                    <div>
-                        <label>Enter Room number: </label>
-                        <input type="number" placeholder="Room no :-" />
-                    </div>
-                </div>
+                
             </div>
         </>
     )
