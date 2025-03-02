d714ab479e9fd4e82367bbb53630caf3f5ca0947
Bishnupada Kamila <121252447+bishnu-kamila@users.noreply.github.com>
2025-03-02 23:49:02 +0530

    update

--- src/Component/Student/Laundry.jsx
+++ src/Component/Student/Laundry.jsx
@@ -4,7 +4,6 @@ const Laundry = ()=>{
             <div classname="h-[50px] w-[35px]">
                 <h1>This is Laundry Page</h1>
                 <p>Any kind of Clothes are washed and pressed here</p>
-                <h2>Thank you</h2>
             </div>
         </>
     )
