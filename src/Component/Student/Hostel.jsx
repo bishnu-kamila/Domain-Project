b638225125fb441ae37631214e39b07b638d7769
Bishnupada Kamila <121252447+bishnu-kamila@users.noreply.github.com>
2025-03-07 23:52:16 +0530

    update

--- src/Component/Student/Hostel.jsx
+++ src/Component/Student/Hostel.jsx
@@ -5,7 +5,7 @@ const Hostel = ()=>{
                 <h1 className="font-bold text-2xl">Hostel</h1>
             </div>
             <div className="flex display-center justify-center mt-[30px]">
-                
+                <h1 className="font-bold text-2xl">Bishnupada Kamila</h1>
             </div>
         </>
     )
